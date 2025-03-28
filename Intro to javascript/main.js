fd0072b40a044a69e3abcd545bdf2ca6dea72256
asif1416 <ashrafulasif260@gmail.com>
2025-03-28 21:19:44 +0600

    functions in javascript

---
+++ Intro to javascript/main.js
@@ -0,0 +1,12 @@
+document.getElementById("myDiv").innerHTML = "<h1>Hello</h1>"
+
+document.getElementById("para").innerText = "Hello";
+
+var a = 10;  //comment
+let b= 20;
+let c = a+b
+let r = 6%5;
+a += 2; 
+
+document.writeln("The sum is: " ,c, "<br>")
+document.writeln(a)
